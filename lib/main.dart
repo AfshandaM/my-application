86d841c77a70d181ef3dc4aaa16c0b6fff4da02e
dell <madrasiafshanda@gmail.com>
2020-10-01 01:35:52 +0530

    First App Screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,23 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(
-    Center(
-      child: Text(
-        "Hello Flutter",
-        textDirection: TextDirection.ltr
-      ),)
 
+    MaterialApp(
+      title: "My Flutter App",
+      home: Scaffold(
+        appBar: AppBar(title: Text("My first App Screen"),),
+        body: Material(
+          color: Colors.teal,
+          child: Center(
+            child: Text(
+              "Hello Flutter",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 40.0),
+            ),
+          ),
+        ),
+      )
+    )
   );
 }
 
